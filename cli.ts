0f74a94bc1d0d651975eb49b397bfdb049d72cf4
ph4u <faunotv@gmail.com>
2022-02-19 21:51:51 -0600

    fix(Root): add unpack on cli.

--- cli.ts
+++ cli.ts
@@ -1,13 +1,15 @@
 import { css_reducer_sync } from "./css_reducer.ts";
 import { brightCyan } from "https://deno.land/std@0.125.0/fmt/colors.ts";
 
-const [file, windi] = Deno.args;
+const [file, ...opts] = Deno.args;
 
-if (file && windi === "--windi") {
+if (file && opts.includes("--windi") && opts.includes("--unpack")) {
+  console.log(css_reducer_sync(file, { unpack: true, windi_shortcuts: true }));
+} else if (file && opts.includes("--unpack")) {
+  console.log(css_reducer_sync(file, { unpack: true }));
+} else if (file && opts.includes("--windi")) {
   console.log(css_reducer_sync(file, { windi_shortcuts: true, cb: ask }));
-} else if (file) {
-  console.log(css_reducer_sync(file, { cb: ask }));
-}
+} else if (file) console.log(css_reducer_sync(file, { cb: ask }));
 
 function ask() {
   const name = prompt(brightCyan("\nRefactor as: "));
